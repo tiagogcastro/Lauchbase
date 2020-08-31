0746e95895b653f82ce6b643a7919202049bff4a
Tiaguin061 <tiaguin180@gmail.com>
2020-08-30 21:11:00 -0300

    Refactoring from module 4 to module 5 and creating and adding to the database

---
+++ src/routes.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const routes = express.Router() // criando a rota dos metodos get
+const instructors = require("./app/controllers/instructors")
+const members = require('./app/controllers/members')
+
+routes.get('/', function (req, res) {
+    return res.redirect('/instructors')
+})
+
+routes.get('/instructors', instructors.index)
+
+routes.get('/instructors/create', instructors.create)
+
+// ve o id
+routes.get('/instructors/:id', instructors.show)
+
+// para ir na page de editar
+routes.get('/instructors/:id/edit', instructors.edit)
+
+// para editar
+routes.put('/instructors', instructors.put)
+
+routes.delete('/instructors', instructors.delete)
+
+routes.post('/instructors', instructors.post) 
+
+
+routes.get('/members', members.index)
+
+routes.get('/members/create', members.create)
+
+routes.get('/members/:id/', members.show)
+
+routes.get('/members/:id/edit', members.edit)
+
+routes.put('/members', members.put)
+
+routes.delete('/members', members.delete)
+
+routes.post('/members', members.post)
+
+module.exports = routes
+
+
+// get: Receber RESOURCE
+// Post: Criar um novo RESOURCER com dados enviados
+// put: Atualizar RESOURCE
+// delete: Deletar RESOURCE
