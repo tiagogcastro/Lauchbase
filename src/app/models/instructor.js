bbaa9f20a38c987fbb36d7dbf7910d15a2e34b68
Tiaguin061 <tiaguin180@gmail.com>
2020-09-03 19:13:03 -0300

    Filtering instructors

--- src/app/models/instructor.js
+++ src/app/models/instructor.js
@@ -62,6 +62,24 @@ module.exports = {
         })
     },
 
+    findBy(filter, callback) {
+        db.query(`
+        SELECT instructors.*, count(members) AS total_students
+        FROM instructors
+        LEFT JOIN members ON (instructors.id = members.instructor_id)
+        WHERE instructors.name ILIKE '%${filter}%'
+        OR instructors.services ILIKE '%${filter}%'
+        GROUP BY (instructors.id)
+        ORDER BY total_students DESC`, 
+        function(err, results) {
+            if(err) {
+                throw `Database Error! ${err}`
+            }
+            
+            callback(results.rows)
+        })
+    },
+
     update(data, callback) {
         const query = `
             UPDATE instructors SET
