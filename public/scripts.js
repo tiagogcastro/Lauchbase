4b69cedf9a5bca798f6134d834d3c4cc066cbbf2
Tiaguin061 <tiaguin180@gmail.com>
2020-09-03 22:31:00 -0300

    Paging algorithm for the frontend

--- public/scripts.js
+++ public/scripts.js
@@ -7,6 +7,39 @@ for (item of menuItems) {
     }
 }
 
+let totalPages = 20,
+    selectedPage = 6, 
+    pages = [],
+    oldPage
+
+for(let currentPage = 1; currentPage <= totalPages; currentPage++) {
+    
+    const firstAndLastPage = currentPage == 1 || currentPage == totalPages
+    const pagesAfterSelectedPage = currentPage <= selectedPage + 2
+    const pagesBeforeSelectedPage = currentPage >= selectedPage - 2
+
+    if(firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
+
+        if(oldPage && currentPage - oldPage > 2) {
+            pages.push('...')
+        }
+
+        if(oldPage && currentPage - oldPage == 2) {
+            pages.push(oldPage + 1)
+        }
+
+        pages.push(currentPage)
+
+        // page q o user esta
+        oldPage = currentPage
+    }
+}
+
+console.log(pages)
+
+
+
+
 const formDelete = document.querySelector('#form-delete')
 formDelete.addEventListener('submit', function(event) {
     const confirmation = confirm('Deseja Deletar?')
@@ -16,3 +49,9 @@ formDelete.addEventListener('submit', function(event) {
         return res.redirect('/')
     }
 })
+
+// Paginate
+
+// totalPages = 20
+// selectedPage = 15
+
