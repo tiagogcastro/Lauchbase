1b849d82b728778138f01d17c5422c88201242e2
Tiaguin061 <tiaguin180@gmail.com>
2020-09-06 16:39:25 -0300

    Saving products with async and await

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,7 +1,8 @@
 const Category = require('../models/Category')
+const Product = require('../models/Product')
 
 module.exports = {
-    create(req, res) {
+    async create(req, res) {
         // Pegar categorias
         Category.all()
         .then(function(results) {
@@ -13,9 +14,23 @@ module.exports = {
         })
     },
 
-    post(req, res) {
+    async post(req, res) {
         // Logica de salvar
+        const keys = Object.keys(req.body) 
+
+        for(key of keys) {
+            if (req.body[key] == "") {
+                return res.send('Please, fill all fields')
+            }
+        }
+
+        // Pegando os produtos e todas as categorias
+        let results = await Product.create(req.body)
+        const productId = results.rows[0].id
+
+        results = await Category.all()
+        const categories = results.rows
+
+        return res.render('products/create.njk', { productId, categories})
     }
-
-
 }
