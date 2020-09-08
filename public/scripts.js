c44aa08423db85d3bef84784185112344602e9e9
Tiaguin061 <tiaguin180@gmail.com>
2020-09-08 18:44:13 -0300

    Managing image uploads with javascript

--- public/scripts.js
+++ public/scripts.js
@@ -14,3 +14,37 @@ const Mask = {
         }).format(value / 100)
     }
 }
+
+const PhotosUpload = {
+    uploadLimit: 6,
+    handleFileInput(event) {
+        const { files: fileList } = event.target
+        const { uploadLimit } = PhotosUpload
+
+        if (fileList.length > uploadLimit) {
+            alert(`Voce enviou ${fileList.length}. Envie no máximo ${uploadLimit} fotos`)
+            event.preventDefault()
+            return
+        }
+
+        Array.from(fileList).forEach((file) => {
+            const reader = new FileReader()
+
+            reader.onload = () => {
+                const image = new Image()
+                image.src = String(reader.result)
+
+                const div = document.createElement('div')
+                div.classList.add('photo')
+
+                div.onclick = () => alert('Remover foto')
+
+                div.appendChild(image)
+
+                document.querySelector('#photos-preview').appendChild(div)
+            }
+
+            reader.readAsDataURL(file)
+        })
+    }
+}
