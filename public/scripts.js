9668876e6008409549d06055329b358cdb594d56
Tiaguin061 <tiaguin180@gmail.com>
2020-09-09 11:28:00 -0300

    Finalizing the image manager on the frontend

--- public/scripts.js
+++ public/scripts.js
@@ -16,15 +16,21 @@ const Mask = {
 }
 
 const PhotosUpload = {
+    input: "",
     preview: document.querySelector('#photos-preview'),
+    files: [],
     uploadLimit: 6,
     handleFileInput(event) {
         const { files: fileList } = event.target
+        PhotosUpload.input = event.target
 
         if (PhotosUpload.hasLimit(event)) return
 
         // Vai colocar a imagem no array
         Array.from(fileList).forEach((file) => {
+
+            PhotosUpload.files.push(file)
+
             const reader = new FileReader()
 
             reader.onload = () => {
@@ -38,12 +44,15 @@ const PhotosUpload = {
 
             reader.readAsDataURL(file)
         })
+
+        // Trocando o comportamento padrao para o nosso
+        PhotosUpload.input.files = PhotosUpload.getAllFiles()
     },
 
     hasLimit(event) {
         // Vai pegar o total de arquivos e verificar o limite
-        const { uploadLimit } = PhotosUpload
-        const { files: fileList } = event.target
+        const { uploadLimit, input, preview} = PhotosUpload
+        const { files: fileList} = input
 
         if (fileList.length > uploadLimit) {
             alert(`Voce enviou ${fileList.length}. Envie no máximo ${uploadLimit} fotos`)
@@ -51,11 +60,35 @@ const PhotosUpload = {
             return true
         }
 
+        // preview é o container e childNodes é os filhos
+        const photosDiv = []
+        preview.childNodes.forEach(item => {
+            if (item.classList && item.classList.value == "photo") {
+                photosDiv.push(item)
+            }
+        })
+
+        const totalPhotos = fileList.length + photosDiv.length
+        if(totalPhotos > uploadLimit) {
+            alert("Limite max")
+            event.preventDefault()
+            return true
+        }
+
         return false
     },
 
-    // Vai pegar e adicionar uma div e class
+    getAllFiles() {
+        const dataTransfer = new ClipboardEvent("").clipboardData || new DataTransfer()
+
+        // Adicionando cada item no dataTransfer ou atualizando
+        PhotosUpload.files.forEach(file => dataTransfer.items.add(file))
+
+        return dataTransfer.files
+    },
+
     getContainer(image) {
+        // Vai pegar e adicionar uma div e class
         const div = document.createElement('div')
         div.classList.add('photo')
 
@@ -76,11 +109,16 @@ const PhotosUpload = {
         return button
     },
 
-    // Quando clicar na foto
     removePhoto(event) {
-        const photoDiv = event.target.parentNode
+        // Quando clicar na foto
+        const photoDiv = event.target.parentNode // <div class="photo">
         const photosArray = Array.from(PhotosUpload.preview.children)
         const index = photosArray.indexOf(photoDiv)
+
+        // aplica no array pra tirar uma posiçao do array
+        PhotosUpload.files.splice(index, 1)
+        // Roda a função de novo
+        PhotosUpload.input.files = PhotosUpload.getAllFiles()
 
         photoDiv.remove()
     }
