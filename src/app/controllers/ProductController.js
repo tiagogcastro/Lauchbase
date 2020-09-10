0ee2a538a8608419dabd5a6b5fb7715d8bbcff48
Tiaguin061 <tiaguin180@gmail.com>
2020-09-10 19:31:36 -0300

    Adding deletion of images from the database by the manager

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -68,7 +68,6 @@ module.exports = {
             src: `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`
         }))
 
-
         return res.render('products/edit.njk', { product, categories, files})
     },
 
@@ -76,10 +75,20 @@ module.exports = {
         const keys = Object.keys(req.body) 
 
         for(key of keys) {
-            if (req.body[key] == "") {
+            if (req.body[key] == "" && key != "removed_files") {
                 return res.send('Please, fill all fields')
             } 
         }
+
+        if (req.body.removed_files) {
+            const removedFiles = req.body.removed_files.split(",") // [1,2,3,]
+            const lastIndex = removedFiles.length - 1
+            removedFiles.splice(lastIndex, 1) // [1,2,3]
+            
+            const removedFilesPromise = removedFiles.map(id => File.delete(id))
+            await Promise.all(removedFilesPromise)
+        }
+
 
         req.body.price = req.body.price.replace(/\D/g, "")
 
