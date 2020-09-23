d9022bd653a592e521b26f71f1b577bb7e2e3d87
Tiaguin061 <tiaguin180@gmail.com>
2020-09-22 23:07:39 -0300

    Frontend email validation

--- public/scripts.js
+++ public/scripts.js
@@ -216,3 +216,48 @@ const Lightbox = {
         Lightbox.closeButton.style.top = "-80px"
     }
 }
+
+const Validate = {
+    apply(input, func) {
+        Validate.clearErrors(input)
+
+        let results = Validate[func](input.value)
+        input.value = results.value
+
+        // Não sai do campo enquanto não por email certo
+        if (results.error) {
+            Validate.displayError(input, results.error)
+        }
+
+    },
+
+    // Create display 
+    displayError(input, error) {
+        const div = document.createElement('div')
+        div.classList.add('error')
+        div.innerHTML = error
+        input.parentNode.appendChild(div)
+        input.focus()
+    },
+
+    clearErrors(input) {
+        const errorDiv = input.parentNode.querySelector('.error')
+
+        if (errorDiv)
+            errorDiv.remove()
+    },
+
+    isEmail(value) {
+        let error = null
+
+        // ab. ou ab-abc@abc.abc
+        const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+        if (!value.match(mailFormat))
+            error = "Email inválido"
+        return {
+            error,
+            value
+        }
+    }
+}
