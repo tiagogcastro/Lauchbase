090b032126d87475ac20f764acb0a53a40a21b40
Tiaguin061 <tiaguin180@gmail.com>
2020-09-24 10:40:44 -0300

    Making error messages from the backend

--- src/app/validators/user.js
+++ src/app/validators/user.js
@@ -1,29 +1,42 @@
 const User = require('../models/User')
 
 async function post(req, res, next) {
-     // chek if has all fields
-     const keys = Object.keys(req.body) 
+    // chek if has all fields
+    const keys = Object.keys(req.body) 
 
-     for(key of keys) {
-         if (req.body[key] == "") {
-             return res.send('Please, fill all fields')
-         }
-     }
+    for(key of keys) {
+        if (req.body[key] == "") {
+            return res.render('user/register', {
+                user: req.body,
+                error: 'Por favor, preencha todos os campos! '
+            })
+        }
+    }
 
-     // check if user exists [email, cpf_cnpj UNIQUE]
-     let { email, cpf_cnpj, password, passwordRepeat} = req.body
-     
-     cpf_cnpj = cpf_cnpj.replace(/\D/g, "")
+    // check if user exists [email, cpf_cnpj UNIQUE]
+    let { email, cpf_cnpj, password, passwordRepeat} = req.body
+    
+    cpf_cnpj = cpf_cnpj.replace(/\D/g, "")
 
-     const user = await User.findOne({ 
-         where: { email },
-         or: { cpf_cnpj }
-     })
+    const user = await User.findOne({ 
+        where: { email },
+        or: { cpf_cnpj }
+    })
 
-     if (user) return res.send('Users exists')
-     // check if password match
-     if (password != passwordRepeat)
-         return res.send('Password no match')
+    if (user) {
+        return res.render('user/register', {
+            user: req.body,
+            error: 'Usuário já cadastrado!'
+        })
+    }
+
+    // check if password match
+    if (password != passwordRepeat) {
+        return res.render('user/register', {
+            user: req.body,
+            error: 'As senhas não são iguais!'
+        })
+    }
 
     next()
 }
